3e31edea169040d3c7b738b1af21db49240d8853
uhziel <uhziel@gmail.com>
2020-04-10 09:00:58 +0800

    支持使用命令行参数

--- cpp_hproto/cpp_hproto.cpp
+++ cpp_hproto/cpp_hproto.cpp
@@ -3,8 +3,25 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
 
 using namespace clang;
+using namespace clang::tooling;
+using namespace llvm;
+
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static cl::OptionCategory CppHprotoCategory("cpp_hproto options");
+
+// CommonOptionsParser declares HelpMessage with a description of the common
+// command-line options related to the compilation database and input files.
+// It's nice to have this help message in all tools.
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+// A help message for this specific tool can be added afterwards.
+static cl::extrahelp MoreHelp("\nMore help text...\n");
 
 class Class2LuaVisitor
   : public RecursiveASTVisitor<Class2LuaVisitor> {
@@ -42,8 +59,9 @@ public:
   }
 };
 
-int main(int argc, char **argv) {
-  if (argc > 1) {
-    clang::tooling::runToolOnCode(new Class2LuaAction, argv[1]);
-  }
+int main(int argc, const char **argv) {
+  CommonOptionsParser OptionsParser(argc, argv, CppHprotoCategory);
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+  Tool.run(newFrontendActionFactory<Class2LuaAction>().get());
 }
